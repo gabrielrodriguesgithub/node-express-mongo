8460651438d08ccd29f3344e64f73b8da6fa2e50
Gabriel Rodrigues <biel.rodriguesvieira@gmail.com>
2024-03-06 23:28:03 -0300

    db mongodb

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-import http from "http";
+import "dotenv/config";
 import app from "./src/app.js";
 
 const PORT = 3000;
 
-
 app.listen(PORT, () => {
     console.log('o servidor esta ouvindo na porta 3000')
 })
-
 
--- src/app.js
+++ src/app.js
@@ -1,24 +1,48 @@
 import express from "express";
+import conectaNaDataBase from "./config/dbconnect.js";
+import livro from "../models/model.js";
+
+const conexao = await conectaNaDataBase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexao: " + erro)
+})
+
+conexao.once('open', () => {
+    console.log('conexao feita ;)')
+})
 
 const app = express();
-
-const livros = [
-    {
-        id: 1,
-        titulo: "Senhor dos Anéis"
-    },
-    {
-        id: 2,
-        titulo: "Harry Potter"
-    }
-]
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Node JS');
 });
 
-app.get('/livros', (req, res) =>{
-    res.status(200).json(livros)
+app.get('/livros', async (req, res) =>{
+    const livros = await livro.find({})
+    res.status(200).json(livros);
+});
+
+app.get('/livros/rocco', async (req, res) => {
+    const editora = 'rocco'
+    const listaLivros = await livro.find({editora});
+    res.status(200).json(listaLivros);
+});
+
+app.put('/livros/:id', async (req, res) => {
+    
+});
+
+app.post('/livros', async (req, res) => {
+    const novoLivro = await livro.create(req.body)
+    res.status(201).json("Livro criado com succeess!!" + novoLivro);
+});
+
+app.delete('/livros/:id', (req, res) => {
+    const index = mostrarIndex(req.params.id);
+    livros.splice(index, 1)
+    res.status(200).send('livro deletado com sucesso!')
 })
 
 export default app;
